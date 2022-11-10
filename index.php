4f1425baf15acf6a2afbd3a62fe8d78e3f1bba6e
David Coello Suárez <117922224+SPTE-SISTEMAS-WEB@users.noreply.github.com>
2022-11-10 13:35:42 -0500

    MENSAJE DE FUNCIONAMIENTO

--- index.php
+++ index.php
@@ -6,12 +6,18 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Propac</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 </head>
 
-<body>
-    <?php
-    phpinfo();
-    ?>
+<body class="container vh-100">
+    <div class="row vh-100">
+        <div class="align-self-center col-6 offset-3">
+            <div class="alert alert-success" role="alert">
+                Confirmación de éxito funcionamiento
+            </div>
+        </div>
+    </div>
+
 </body>
 
 </html>
